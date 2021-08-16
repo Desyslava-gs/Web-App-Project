7b2834ed2af3b074e1360c3f44f762661d62a9c2
Desyslava-gs <84912601+Desyslava-gs@users.noreply.github.com>
2021-08-16 15:03:56 +0300

    Add

---
+++ WebApp/Models/Api/Statistics/StatisticsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models.Api.Statistics
+{
+    public class StatisticsModel
+    {
+        public int AllCars { get; set; }
+        public int AllRepairs { get; set; }
+        public int FinishedRepairs { get; set; }
+        public int AllClients { get; set; }
+    }
+}
